3262abdc50e398468c6ee3e7a75ac3c8e2f78bf3
nachopep10 <ignaciopepevalverde31@gmail.com>
2025-08-26 01:56:48 -0300

    tarea

---
+++ frontend/src/app/pages/sobremi/sobremi.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobremi',
+  imports: [],
+  templateUrl: './sobremi.html',
+  styleUrl: './sobremi.css'
+})
+export class Sobremi {
+
+}
